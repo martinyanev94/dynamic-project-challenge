ba56be60e6efd4c6322ca01630e95ebfcc7533c1
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-28 01:38:52 -0400

    Add Lesson_14_Advanced_Debugging_Techniques/part_2_example_script.bash

---
+++ Lesson_14_Advanced_Debugging_Techniques/part_2_example_script.bash
@@ -0,0 +1,10 @@
+./your_script.sh
+#!/bin/bash
+
+my_var="Hello"
+echo "Starting the script"
+echo "Current value of my_var: $my_var"
+# Simulate some script processing
+my_var="World"
+echo "Current value of my_var: $my_var"
+echo "Ending the script"
