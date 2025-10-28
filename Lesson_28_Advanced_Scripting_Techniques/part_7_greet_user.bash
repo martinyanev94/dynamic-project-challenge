b095190b46204e6560f8414753157511a7114cd5
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-28 01:41:41 -0400

    Add Lesson_28_Advanced_Scripting_Techniques/part_7_greet_user.bash

---
+++ Lesson_28_Advanced_Scripting_Techniques/part_7_greet_user.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script greets the user based on their input
+
+echo "What is your name?" # Prompt for user name
+read user_name # Store input in variable
+echo "Hello, $user_name! Welcome to the Bash scripting world." # Greet user
