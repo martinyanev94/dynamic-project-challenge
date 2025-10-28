475b917902ea7ffb86d051d662c5a0317ee24907
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-28 01:38:45 -0400

    Add Lesson_13_Basics_Of_Bash_Scripting/part_3_hello_world.bash

---
+++ Lesson_13_Basics_Of_Bash_Scripting/part_3_hello_world.bash
@@ -0,0 +1,20 @@
+./hello_world.sh John
+#!/bin/bash
+name=$1
+echo "Hello, $name!"
+./hello_world.sh John
+
+bash
+./hello_world.sh John
+
+bash
+#!/bin/bash
+name=$1
+echo "Hello, $name!"
+
+bash
+./hello_world.sh John
+
+
+Hello, John!
+
