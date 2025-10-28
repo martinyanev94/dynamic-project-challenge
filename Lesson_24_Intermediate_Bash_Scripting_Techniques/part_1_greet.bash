eb07102bbada9287c2f1537548363e0d838fc20d
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-28 01:40:50 -0400

    Add Lesson_24_Intermediate_Bash_Scripting_Techniques/part_1_greet.bash

---
+++ Lesson_24_Intermediate_Bash_Scripting_Techniques/part_1_greet.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Hello, $1! Welcome to the world of Bash scripting!"
+./greet.sh Alice
+
+bash
+#!/bin/bash
+
+echo "Hello, $1! Welcome to the world of Bash scripting!"
+
+bash
+./greet.sh Alice
+
+
+Hello, Alice! Welcome to the world of Bash scripting!
+
