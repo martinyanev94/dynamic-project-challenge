dee21b02df76f01479ab5f1fbbfe63841d6e7b31
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-28 01:42:04 -0400

    Add Lesson_30_Creating_Interactive_Scripts/part_1_interactive_script.bash

---
+++ Lesson_30_Creating_Interactive_Scripts/part_1_interactive_script.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "What's your name?"
+read name
+echo "Hello, $name! Welcome to the interactive script world!"
+What's your name?
+Alice
+Hello, Alice! Welcome to the interactive script world!
