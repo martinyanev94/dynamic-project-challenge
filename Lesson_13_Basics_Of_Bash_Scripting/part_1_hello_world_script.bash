c07e80391a6312852f68c542ffa31cf6abe65fda
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-28 01:38:42 -0400

    Add Lesson_13_Basics_Of_Bash_Scripting/part_1_hello_world_script.bash

---
+++ Lesson_13_Basics_Of_Bash_Scripting/part_1_hello_world_script.bash
@@ -0,0 +1,24 @@
+nano hello_world.sh
+#!/bin/bash
+echo "Hello, World!"
+chmod +x hello_world.sh
+./hello_world.sh
+
+bash
+nano hello_world.sh
+
+bash
+#!/bin/bash
+
+bash
+echo "Hello, World!"
+
+bash
+chmod +x hello_world.sh
+
+bash
+./hello_world.sh
+
+
+Hello, World!
+
