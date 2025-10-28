f430842ccce8746c170680ddd53c49ac35a4aa88
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-28 01:40:55 -0400

    Add Lesson_24_Intermediate_Bash_Scripting_Techniques/part_4_interactive_greeting.bash

---
+++ Lesson_24_Intermediate_Bash_Scripting_Techniques/part_4_interactive_greeting.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "What is your name?"
+read name
+
+echo "How old are you?"
+read age
+
+echo "Hello, $name! You are $age years old. Welcome to the world of Bash scripting!"
+./interactive_greet.sh
+Alice
+How old are you?
+25
+Hello, Alice! You are 25 years old. Welcome to the world of Bash scripting!
